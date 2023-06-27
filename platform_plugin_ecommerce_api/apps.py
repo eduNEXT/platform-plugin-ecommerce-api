f5e248b91cb47eae84d976470ba8a9af0916b127
Maria Fernanda Magallanes Zubillaga <maria.magallanes@edunext.co>
2023-06-26 22:44:51 -0500

    ✨ First commit

---
+++ platform_plugin_ecommerce_api/apps.py
@@ -0,0 +1,13 @@
+"""
+platform_plugin_ecommerce_api Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class PlatformPluginEcommerceApiConfig(AppConfig):
+    """
+    Configuration for the platform_plugin_ecommerce_api Django application.
+    """
+
+    name = 'platform_plugin_ecommerce_api'
