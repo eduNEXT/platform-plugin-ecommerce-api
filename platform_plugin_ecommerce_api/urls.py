3f5ac8a57e53c9c79ba0ce806ec2a67912858b25
Andres Espinel <andressespinel@hotmail.com>
2023-07-23 20:38:21 -0500

    feat: Add basic structure of the course enrollment API

--- platform_plugin_ecommerce_api/urls.py
+++ platform_plugin_ecommerce_api/urls.py
@@ -4,13 +4,10 @@ URLs for platform_plugin_ecommerce_api.
 from django.urls import re_path  # pylint: disable=unused-import
 from django.views.generic import TemplateView  # pylint: disable=unused-import
 from django.urls import path,include
-from platform_plugin_ecommerce_api.views import APIConfigView
+from platform_plugin_ecommerce_api.views import CourseEnrollmentView
 from django.conf.urls import url
 
 urlpatterns = [
-    # other URL patterns for your main app,
-    path('api/', include([
-        path('user/', APIConfigView.as_view(), name='edxapp-user'),
-    ])),
+    url(r'^enrollment/$', CourseEnrollmentView.as_view(), name='enrollment'),
 ]
 
